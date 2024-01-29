7ca1ac535122ad947f1e19b5b6ef485ee1209697
hactuallybenji <mrbenamiller@gmail.com>
2024-01-29 12:41:30 -0800

    Add red led for bad weather

--- weather.py
+++ weather.py
@@ -1,11 +1,12 @@
 import machine
 
 green_led = machine.Pin(15, machine.Pin.OUT)
-green_led.off()
 green_led.on()
 
 yellow_led = machine.Pin(14, machine.Pin.OUT)
 yellow_led.on()
 
+red_led = machine.Pin(13, machine.Pin.OUT)
+red_led.on()
 
-weather_url = 
+red_led.toggle()
